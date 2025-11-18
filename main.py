0238d5f97669e11f585c79661c1989cd78201532
Arnis Armans <yl3gbc@yl3gbc.lv>
2025-11-19 00:09:00 +0200

    pieliktti pirmi faili

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/home/")
+def home():
+    return "PR.Andra Solima mŪzikas lapa"
+
+@app.get("/notis/")
+def notis():
+    return ("Notis")
+
+@app.get("/autori/{autori_id}")
+def autori(autori_id):
+    return f"Autori  {autori_id}"
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
